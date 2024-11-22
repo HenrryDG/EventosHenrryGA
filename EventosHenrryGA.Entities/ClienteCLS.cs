028125a1b93039085d742441622f35855a2dad38
Henrry <148930610+HenrryDG@users.noreply.github.com>
2024-11-22 10:34:58 -0400

    Reemplazo de formulario y ajustes de CSS
    Se ha reemplazado el formulario HTML estándar por un componente `EditForm` de Blazor con validación de datos mediante `DataAnnotationsValidator`. Se han añadido mensajes de validación específicos para los campos `Nombre`, `Apellido Paterno` y `Apellido Materno` utilizando el componente `ValidationMessage`. El botón de guardar ahora utiliza el evento `OnValidSubmit` del `EditForm`.
    
    En `app.output.css`, se han añadido y eliminado varias clases CSS para ajustar márgenes, padding, altura, ancho, colores de fondo, colores de texto, sombras y otros estilos. Se han añadido nuevas clases CSS para gestionar el estado de hover y focus en diferentes elementos, así como para soportar el modo oscuro.
    
    Se han añadido validaciones de datos en la clase `ClienteCLS` en el archivo `ClienteCLS.cs`, incluyendo restricciones de longitud y expresiones regulares para los campos `Nombre`, `Apellido Paterno` y `Apellido Materno`.

--- EventosHenrryGA.Entities/ClienteCLS.cs
+++ EventosHenrryGA.Entities/ClienteCLS.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,24 @@ namespace EventosHenrryGA.Entities
     public class ClienteCLS
     {
         public int Id { get; set; }
+
+
+
+        // VALIDACIONES
+
+        [Required(ErrorMessage = "Debes introducir un nombre")]
+        [StringLength(50, ErrorMessage = "La longitud máxima del nombre es 50")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚüÜ ]*$", ErrorMessage = "Solo se permiten letras")]
         public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "Debes introducir el apellido paterno")]
+        [StringLength(50, ErrorMessage = "La longitud máxima del apellido es 50")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚüÜ ]*$", ErrorMessage = "Solo se permiten letras")]
         public string? ApellidoPaterno { get; set; }
+
+        [Required(ErrorMessage = "Debes introducir el apellido paterno")]
+        [StringLength(50, ErrorMessage = "La longitud máxima del apellido es 50")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚüÜ ]*$", ErrorMessage = "Solo se permiten letras")]
         public string? ApellidoMaterno { get; set; }
     }
 }
