427521261b914bb42637873594f43bd104a35048
Henrry <148930610+HenrryDG@users.noreply.github.com>
2024-11-22 15:50:45 -0400

    Mejoras en EventosList y validación de ClienteCLS
    - Inyectado `EventoService` en `EventosList.razor`.
    - Añadida barra de búsqueda y botones en `EventosList.razor`.
    - Añadido botón para crear nuevo evento en `EventosList.razor`.
    - Modificada tabla de eventos con margen superior (`mt-11`).
    - Añadida columna "Cliente" en la tabla de eventos.
    - Cambiada variable en bucle `foreach` a `eventosFiltrados`.
    - Añadida propiedad `NombreCliente` en la tabla de eventos.
    - Inicializada lista `eventos` y añadida `eventosFiltrados`.
    - Añadida propiedad `eventoBuscado` para texto de búsqueda.
    - Añadidos métodos `Buscar` y `Limpiar` en `EventosList.razor`.
    - Modificada regex para `ApellidoMaterno` en `ClienteCLS` para incluir 'ñ'.

--- EventosHenrryGA.Entities/ClienteCLS.cs
+++ EventosHenrryGA.Entities/ClienteCLS.cs
@@ -12,9 +12,6 @@ namespace EventosHenrryGA.Entities
         public int Id { get; set; }
 
 
-
-        // VALIDACIONES
-
         [Required(ErrorMessage = "Debes introducir un nombre")]
         [StringLength(50, ErrorMessage = "La longitud máxima del nombre es 50")]
         [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚüÜ ]*$", ErrorMessage = "Solo se permiten letras")]
@@ -27,7 +24,7 @@ namespace EventosHenrryGA.Entities
 
         [Required(ErrorMessage = "Debes introducir el apellido paterno")]
         [StringLength(50, ErrorMessage = "La longitud máxima del apellido es 50")]
-        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚüÜ ]*$", ErrorMessage = "Solo se permiten letras")]
+        [RegularExpression("^[a-zñA-ZáéíóúÁÉÍÓÚüÜ ]*$", ErrorMessage = "Solo se permiten letras")]
         public string? ApellidoMaterno { get; set; }
     }
 }
